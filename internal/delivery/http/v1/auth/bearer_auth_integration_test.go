4e7a30bbce8cf4892aca2a7b2146b093afd10a57
Artem Pistryi <gazated@gmail.com>
2022-11-20 19:13:20 +0400

    fix: corrected bearer_auth_integration_test.go

--- internal/delivery/http/v1/auth/bearer_auth_integration_test.go
+++ internal/delivery/http/v1/auth/bearer_auth_integration_test.go
@@ -17,38 +17,37 @@ var validKey = []byte("1234")
 var invalidKey = []byte("42-42-42")
 
 func getParametrizedTokenHandler(key []byte) *security.JwtHandler {
-	jwt := security.NewJwtHandler()
-
+	jwt := security.NewJwtHandler(key)
+	
 	const aud = "course-watch-api"
-
+	
 	jwt.Issuer = "course-watch"
 	jwt.AudienceExpected = aud
 	jwt.AudienceGenerated = []string{aud}
-	jwt.SigningKey = key
 	jwt.TokenTtl = time.Hour * 1
-
+	
 	return jwt
 }
 
 func TestBearerAuthenticator_Integration_Authorize(t *testing.T) {
 	tokenHandler := getParametrizedTokenHandler(validKey)
 	fakeTokenHandler := getParametrizedTokenHandler(invalidKey)
-
+	
 	ba := auth.NewBearerAuthenticator(tokenHandler)
-
+	
 	var endpointHit bool
-
+	
 	router := gin.New()
 	g := router.Group("/secure", ba.Authorize(security.Admin))
 	g.GET("/data", func(context *gin.Context) {
 		endpointHit = true
-
+		
 		up, err := auth.GetAuthenticatedUser(context)
 		require.NoError(t, err)
-
+		
 		context.JSON(http.StatusOK, up)
 	})
-
+	
 	cases := map[string]struct {
 		loggedInUser         *security.UserPrincipal
 		tokenHandler         *security.JwtHandler
@@ -89,12 +88,12 @@ func TestBearerAuthenticator_Integration_Authorize(t *testing.T) {
 			expectedErrorMessage: `{"message":"Forbidden. Required user role: admin"}`,
 		},
 	}
-
+	
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-
+			
 			endpointHit = false
-
+			
 			w := httptest.NewRecorder()
 			req, _ := http.NewRequest(http.MethodGet, "/secure/data", nil)
 			if tc.loggedInUser != nil {
@@ -103,9 +102,9 @@ func TestBearerAuthenticator_Integration_Authorize(t *testing.T) {
 				req.Header.Add("Authorization", "Bearer "+tokenString)
 			}
 			router.ServeHTTP(w, req)
-
+			
 			assert.Equal(t, tc.expectedStatusCode, w.Code)
-
+			
 			if tc.expectedStatusCode == http.StatusOK {
 				assert.True(t, endpointHit)
 				var userPrincipal security.UserPrincipal
@@ -123,22 +122,22 @@ func TestBearerAuthenticator_Integration_Authorize(t *testing.T) {
 func TestBearerAuthenticator_Integration_Authenticate(t *testing.T) {
 	tokenHandler := getParametrizedTokenHandler(validKey)
 	fakeTokenHandler := getParametrizedTokenHandler(invalidKey)
-
+	
 	ba := auth.NewBearerAuthenticator(tokenHandler)
-
+	
 	var endpointHit bool
-
+	
 	router := gin.New()
 	g := router.Group("/secure", ba.Authenticate)
 	g.GET("/data", func(context *gin.Context) {
 		endpointHit = true
-
+		
 		up, err := auth.GetAuthenticatedUser(context)
 		require.NoError(t, err)
-
+		
 		context.JSON(http.StatusOK, up)
 	})
-
+	
 	cases := map[string]struct {
 		loggedInUser         *security.UserPrincipal
 		tokenHandler         *security.JwtHandler
@@ -179,12 +178,12 @@ func TestBearerAuthenticator_Integration_Authenticate(t *testing.T) {
 			expectedErrorMessage: "",
 		},
 	}
-
+	
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-
+			
 			endpointHit = false
-
+			
 			w := httptest.NewRecorder()
 			req, _ := http.NewRequest(http.MethodGet, "/secure/data", nil)
 			if tc.loggedInUser != nil {
@@ -193,9 +192,9 @@ func TestBearerAuthenticator_Integration_Authenticate(t *testing.T) {
 				req.Header.Add("Authorization", "Bearer "+tokenString)
 			}
 			router.ServeHTTP(w, req)
-
+			
 			assert.Equal(t, tc.expectedStatusCode, w.Code)
-
+			
 			if tc.expectedStatusCode == http.StatusOK {
 				assert.True(t, endpointHit)
 				var userPrincipal security.UserPrincipal
